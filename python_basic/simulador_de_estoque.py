50ac200580c8e2c023b21564404ed85c0f1304f7
fabioaraujob2b <fabioaraujob2b@gmail.com>
2025-03-20 16:03:05 -0300

    Subindo

---
+++ python_basic/simulador_de_estoque.py
@@ -0,0 +1,5 @@
+#Objetivo desse script é simular um estoque de produtos, onde o usuário pode adicionar, remover, listar e alterar produtos.    
+#FAZER DEPOIS
+produto = []
+quantidade = []
+preco = []
